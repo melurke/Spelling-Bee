d97075516bb528c051f4e71a0ad6c72537448628
melurke <101022215+melurke@users.noreply.github.com>
2022-06-12 10:54:57 +0200

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -1,33 +1,14 @@
-def num_of_letters(word):
-    letters = []
-    for letter in word:
-        if not letter in letters:
-            letters.append(letter)
-    return(len(letters))
+word_list = []
+word_file = open("word_list_confirmed.txt","r")
+for word in word_file:
+    word_list.append(word.strip())
+word_file.close()
 
-file = open("word_list.txt","r")
-word_list = file.readlines()
-file.close()
-print(len(word_list))
-
-for i in range(0, len(word_list)):
-    word_list[i] = word_list[i].strip()
-
-word_list_new = []
-
-for word in word_list:
-    if num_of_letters(word) > 7:
-        word_list_new.append(word)
-
-for word in word_list_new:
-    word_list.remove(word)
-
+word_list = list(dict.fromkeys(word_list))
+word_list.sort()
 words = ""
-
-for i in range(0, len(word_list)):
-    word_list[i] = word_list[i] + "\n"
-    words += word_list[i]
-
-file = open("word_list.txt","w")
-file.write(words)
-file.close()
+for word in word_list:
+    words += word + "\n"
+word_file = open("word_list_confirmed.txt","w")
+word_file.write(words)
+word_file.close()
