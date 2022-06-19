595954bf922dd9ecf450c0df28969e7b176adfe0
melurke <standartbrostv@gmail.com>
2022-06-19 10:35:09 +0200

    Update Word Lists

--- cleaning.py
+++ cleaning.py
@@ -1,14 +1,14 @@
 word_list = []
-word_file = open("word_list_confirmed.txt","r")
-for word in word_file:
-    word_list.append(word.strip())
-word_file.close()
+
+with open('word_list_confirmed.txt', 'r') as word_file:
+    for word in word_file:
+        word_list.append(word.strip())
 
 word_list = list(dict.fromkeys(word_list))
 word_list.sort()
 words = ""
 for word in word_list:
     words += word + "\n"
-word_file = open("word_list_confirmed.txt","w")
-word_file.write(words)
-word_file.close()
+
+with open('word_list_confirmed.txt', 'w') as word_file:
+    word_file.write(words)
